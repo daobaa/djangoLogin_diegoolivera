db7ca4bdb9498facac6a7a1442a3b8102f162f32
Diego Andres Olivera Abarca <2024_diego.olivera@iticbcn.cat>
2025-03-12 18:56:24 +0100

    Config updated

--- ACTIVITAT_14/loginProject/loginProject/settings.py
+++ ACTIVITAT_14/loginProject/loginProject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'logIn.apps.LoginConfig'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,9 @@ ROOT_URLCONF = 'loginProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +78,12 @@ WSGI_APPLICATION = 'loginProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "",
+        'USER': "",
+        'PASSWORD': "",
+        'HOST': "127.0.0.1",
+        'PORT': "5432"
     }
 }
 
