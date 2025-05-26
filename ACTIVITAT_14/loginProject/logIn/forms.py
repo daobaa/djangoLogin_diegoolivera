d5c017ce138705346e1989b9cfa10416c8716447
Diego Andres Olivera Abarca <2024_diego.olivera@iticbcn.cat>
2025-05-26 10:19:26 +0200

    login working diego@diego.com diego

--- ACTIVITAT_14/loginProject/logIn/forms.py
+++ ACTIVITAT_14/loginProject/logIn/forms.py
@@ -6,15 +6,3 @@ User = get_user_model()
 class EmailAuth(forms.Form):
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
-
-    def clean(self):
-        email = self.cleaned_data.get("email")
-        password = self.cleaned_data.get("password")
-
-        if email and password:
-            try:
-                user = User.objects.get(email=email)
-            except User.DoesNotExist:
-                raise forms.ValidationError("Este email no está registrado.")
-
-        return self.cleaned_data
--- ACTIVITAT_14/loginProject/logIn/views.py
+++ ACTIVITAT_14/loginProject/logIn/views.py
@@ -2,7 +2,8 @@ from tabnanny import check
 
 from django.shortcuts import render, redirect
 from django.template import loader
-from django.contrib.auth import login
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.contrib.auth.hashers import check_password
 from .forms import EmailAuth
@@ -15,21 +16,19 @@ def user_form(request):
     if request.method == "POST":
         form = EmailAuth(request.POST)
         if form.is_valid():
-            email = form.cleaned_data.get('email')
-            password = form.cleaned_data.get('password')
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
             try:
-                profile = Profile.objects.get(email=email)
+                user = User.objects.get(email=email)
+            except User.DoesNotExist:
+                return render(request, 'form.html', {'form': form, 'error': 'Email no registrado.'})
 
-                if profile.password == password:
-                    request.session['profile_id'] = profile.id
-                    request.session['authenticated'] = True
-                    return redirect('index')
-                else:
-                    return render(request, 'form.html', {'form': form, 'error': 'Email o contraseña incorrectas.'})
-            except Profile.DoesNotExist:
-                print(f"No existe perfil para este email: {email}")
-                return render(request, 'form.html', {'form': form, 'error': 'Email o contraseña incorrectas.'})
+            user_auth = authenticate(username=user.username, password=password)
+            if user_auth:
+                login(request, user_auth)
+                return redirect('index')
+            else:
+                return render(request, 'form.html', {'form': form, 'error': 'Contraseña incorrecta.'})
     else:
         form = EmailAuth()
-    context = {'form': form}
-    return render(request, 'form.html', context)
+    return render(request, 'form.html', {'form': form})
