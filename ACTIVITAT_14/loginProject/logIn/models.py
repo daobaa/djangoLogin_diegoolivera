eb7c026e826a1ada847cb1a7be46af0248e44cf2
Diego Andres Olivera Abarca <2024_diego.olivera@iticbcn.cat>
2025-05-26 11:38:01 +0200

    LogIn Online v2

--- ACTIVITAT_14/loginProject/logIn/models.py
+++ ACTIVITAT_14/loginProject/logIn/models.py
@@ -6,7 +6,6 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     email = models.EmailField(null=True, blank=True)
-    password = models.CharField(max_length=128, null=True, blank=True)
 
     def __str__(self):
         return f"Profile for {self.user.username}"
